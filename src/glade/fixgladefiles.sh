6f2bb49f3fa999778cd754610a5500dedf679cb9
Mariusz Adamski <mariusz.adamski@gmail.com>
2010-09-18 10:41:01 +0200

    fixgladefiles.sh now fixes files in a loop
    Signed-off-by: Mariusz Adamski <mariusz.adamski@gmail.com>

--- src/glade/fixgladefiles.sh
+++ src/glade/fixgladefiles.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 # Use this script before extracting translatable strings from glade files.
-sed -i -e 's#<property name="title">#<property name="title" translatable="yes">#' $1
+# It changes every title property to translatable.
+
+REGEXP='s#<property name="title">#<property name="title" translatable="yes">#'
+
+for i in *.glade; do
+  echo "Fixing: $i"
+  sed -i -e "$REGEXP" $i
+done
